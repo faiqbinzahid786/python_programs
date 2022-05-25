4bc2d32d39035f5726ed879318cb065bb3a9a65b
Faiq Bin Zahid <36101045+faiqbinzahid786@users.noreply.github.com>
2022-05-25 16:03:10 +0700

    Create PDFtoJPG_JPGtoPDF.py

---
+++ PDFtoJPG_JPGtoPDF.py
@@ -0,0 +1,58 @@
+#Importing libraries
+from pdf2image import convert_from_path
+import os
+import os.path
+from PIL import Image
+from fpdf import FPDF
+
+#Iterating through each page inj the pdf file
+def convert_pdf_to_images(pdf_path):
+    images = convert_from_path(pdf_path)
+    for index, image in enumerate(images):
+        image.save(f'output5/{pdf_path}-{index}.jpg')
+
+#Using the library in the module to convert pdf to png
+def convert():
+    convert_pdf_to_images('tags_1100-1199_12.0cm_n4.pdf')
+   
+#Function to crop the images    
+def crop_center(pil_img, crop_width, crop_height):
+    img_width, img_height = pil_img.size
+    return pil_img.crop(((img_width - crop_width) // 2,
+                         (img_height - crop_height) // 2,
+                         (img_width + crop_width) // 2,
+                         (img_height + crop_height) // 2))
+
+
+#Main fucntion (converting pdf to png and then cropping png files 
+#and replacing it with cropped files)
+if __name__ == "__main__":
+    convert()
+    
+    f = r'C:\Users\Aaron\.spyder-py3\output6'
+    
+    for file in os.listdir(f):
+        f_img = f+"/"+file
+        img = Image.open(f_img)
+        im_new = crop_center(img, 1050, 1050)
+        im_new.save(f_img, quality=95)
+
+        pdf = FPDF()
+        sdir = "output4/"
+        w,h = 0,0
+        
+        for i in range(1, 100):
+            fname = sdir + "tags_1100-1199_12.0cm_n4.pdf-%.1d.png" % i
+            if os.path.exists(fname):
+                if i == 1:
+                    cover = Image.open(fname)
+                    w,h = cover.size
+                    pdf = FPDF(unit = "pt", format = [w,h])
+                image = fname
+                pdf.add_page()
+                pdf.image(image,0,0,w,h)
+            else:
+                print("File not found:", fname)
+        pdf.output("cropped.pdf", "F")
+        print("Image croppped!")
+    
